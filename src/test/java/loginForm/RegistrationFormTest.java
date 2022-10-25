df0aedb1937b8acb2fa7bad6c1e9ff94f0b7e726
JanaZhakPL <janazhakpl@gmail.com>
2022-10-25 23:06:04 +0200

    Homework 11 part 2

--- src/test/java/loginForm/RegistrationFormTest.java
+++ src/test/java/loginForm/RegistrationFormTest.java
@@ -1,53 +1,58 @@
 package loginForm;
 
 
-import org.junit.Assert;
-
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-
-public class RegistrationFormTest extends Base {
-    WebElement firstName = driver.findElement(By.cssSelector("fieldset[class='fieldset'] input[name='name']"));
-    WebElement lastName = driver.findElement(By.xpath("//form[@id='register_form']/fieldset/p[2]/input"));
-    WebElement country = driver.findElement(By.xpath("//form[@id='register_form']/fieldset[7]/input"));
-    WebElement hobby = driver.findElement(By.xpath("//fieldset[3]/div/label/input"));
-    WebElement phone = driver.findElement(By.cssSelector("fieldset[class='fieldset'] input[name='phone']"));
-    WebElement username = driver.findElement(By.cssSelector("fieldset[class='fieldset'] input[name='username']"));
-    WebElement email = driver.findElement(By.cssSelector("fieldset[class='fieldset'] input[name='email']"));
-    WebElement password = driver.findElement(By.xpath("//form[@id='register_form']/fieldset[11]/input"));
-    WebElement confirmPassword = driver.findElement(By.xpath("//form[@id='register_form']/fieldset[12]/input"));
-    WebElement submitBtn = driver.findElement(By.cssSelector("input[value='submit']"));
-    WebElement heading = driver.findElement(By.cssSelector(".heading"));
-    WebElement invalidInput = driver.findElement(By.cssSelector("input:invalid"));
-    //WebElement requiredField = driver.findElement(By.className("error_p"));
+import org.junit.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
+public class RegistrationFormTest {
+    private WebDriver driver;
+    private RegistrationPage registrationPage;
+    @Before
+    public  void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://www.way2automation.com/way2auto_jquery/registration.php#load_box");
+        driver.manage().window().maximize();
+        registrationPage = new RegistrationPage(driver);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
     @Test
     public void ifEmptyForm() throws InterruptedException {
-        submitBtn.click();
-        WebElement requiredField = driver.findElement(By.className("error_p"));
-        Assert.assertTrue(requiredField.isDisplayed());
+        registrationPage.submitBtn.click();
+        Assert.assertTrue(registrationPage.requiredField.isDisplayed());
         Thread.sleep(3000);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("console.log('Submit empty form')");
+        js.executeScript("window.scroll(0,600)");
     }
 
 //    @Test
-//    public void chekEmailForm() {
-//
+//    public void isValidEmail() {
+//        String emailPattern = "^(.+)@(\\\\S+)$";
+//        Pattern p = Pattern.compile(emailPattern);
+//        registrationPage.email.sendKeys("user@user.com");
+//        Matcher m = p.matcher((CharSequence) registrationPage.email);
+//        Assert.assertTrue(m.matches());
 //    }
 
     @Test
     public void isSuccessfullyLogin() {
-        firstName.sendKeys("UserName");
-        lastName.sendKeys("UserLastName");
-        hobby.click();
-        phone.sendKeys("12345678");
-        username.sendKeys("user");
-        email.sendKeys("user@user.com");
-        password.sendKeys("1111");
-        confirmPassword.sendKeys("1111");
-        submitBtn.click();
+        registrationPage.firstName.sendKeys("UserName");
+        registrationPage.lastName.sendKeys("UserLastName");
+        registrationPage.hobby.click();
+        registrationPage.phone.sendKeys("12345678");
+        registrationPage.userName.sendKeys("user");
+        registrationPage.email.sendKeys("user@user.com");
+        registrationPage.password.sendKeys("1111");
+        registrationPage.confirmPassword.sendKeys("1111");
+        registrationPage.submitBtn.click();
     }
 
 //    @Test
---
+++ src/test/java/loginForm/RegistrationPage.java
@@ -0,0 +1,48 @@
+package loginForm;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+//https://www.way2automation.com/way2auto_jquery/registration.php#load_box
+
+public class RegistrationPage {
+        @FindBy(css = "fieldset[class='fieldset'] input[name='name']")
+    public WebElement firstName;
+
+    @FindBy(xpath = "//form[@id='register_form']/fieldset/p[2]/input")
+    public WebElement lastName;
+
+    @FindBy(xpath = "//form[@id='register_form']/fieldset[7]/input")
+    public WebElement country;
+
+    @FindBy(xpath = "//fieldset[3]/div/label/input")
+    public WebElement hobby;
+
+    @FindBy(css = "fieldset[class='fieldset'] input[name='phone']")
+    public WebElement phone;
+
+    @FindBy(css = "fieldset[class='fieldset'] input[name='username']")
+    public WebElement userName;
+
+    @FindBy(css = "fieldset[class='fieldset'] input[name='email']")
+    public WebElement email;
+
+    @FindBy(xpath = "//form[@id='register_form']/fieldset[11]/input")
+    public WebElement password;
+
+    @FindBy(xpath = "//form[@id='register_form']/fieldset[12]/input")
+    public WebElement confirmPassword;
+
+    @FindBy(css = "input[value='submit']")
+    public WebElement submitBtn;
+
+    @FindBy(className = "error_p")
+    public WebElement requiredField;
+
+    public RegistrationPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+}
+
