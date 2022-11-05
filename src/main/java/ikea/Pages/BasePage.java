99194eb215193eb9aa5b77dc8662be663a81f774
JanaZhakPL <janazhakpl@gmail.com>
2022-11-05 19:34:34 +0100

    Homework 15

--- src/main/java/ikea/Pages/BasePage.java
+++ src/main/java/ikea/Pages/BasePage.java
@@ -6,12 +6,16 @@ import org.openqa.selenium.WebElement;
 
 public class BasePage {
     protected WebDriver driver;
+
     private WebElement find(By locator) {
         return driver.findElement(locator);
     }
+
     protected void click(By locator) {
         find(locator).click();
     }
+
     protected void type(By locator, String text) {
         find(locator).sendKeys(text);
-    }}
+    }
+}
